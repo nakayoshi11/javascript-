1f76a88581cf1d7da63168cd40837a2873471255
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2022-03-11 16:41:45 +0900

    リスト色変更commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 window.addEventListener('load', function(){
-
+  // 要素.addEventListener('イベント名', 関数)
   const pullDownButton = document.getElementById("lists")
-  console.log(pullDownButton)
+
+  pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    console.log("mouseout OK")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    console.log("click OK")
+  })
+
 })
