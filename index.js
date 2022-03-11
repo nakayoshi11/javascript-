fe0ebdb80ad41caae585c81e8a9c11d4b5e5d512
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2022-03-11 17:21:18 +0900

    list表示commit

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 window.addEventListener('load', function(){
   // 要素.addEventListener('イベント名', 関数)
   const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
 
   pullDownButton.addEventListener('mouseover', function(){
     // 要素.addEventListener('イベント名', 関数)
-    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
+    this.setAttribute("style", "background-color:#FFBEDA;")
   })
 
   pullDownButton.addEventListener('mouseout', function(){
     // 要素.removeAttribute(name, value)
-    pullDownButton.removeAttribute("style", "background-color:#FFBEDA;")
-    console.log("mouseout OK")
+    this.removeAttribute("style", "background-color:#FFBEDA;")
   })
 
   pullDownButton.addEventListener('click', function() {
-    console.log("click OK")
+    pullDownParents.setAttribute("style", "display:block;")
   })
 
 })
