ccef0a458e6cbd9f93e0d9be92e7ba279a7cda99
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2022-03-11 16:55:47 +0900

    list表示commit

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ window.addEventListener('load', function(){
   const pullDownButton = document.getElementById("lists")
 
   pullDownButton.addEventListener('mouseover', function(){
+    // 要素.addEventListener('イベント名', 関数)
     pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
   })
 
   pullDownButton.addEventListener('mouseout', function(){
+    // 要素.removeAttribute(name, value)
+    pullDownButton.removeAttribute("style", "background-color:#FFBEDA;")
     console.log("mouseout OK")
   })
 
