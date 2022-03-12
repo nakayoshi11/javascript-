0a8206d0830fa8f01219f75c564c6859a31094c5
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2022-03-12 13:04:54 +0900

    完成commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-window.addEventListener('load', function(){
+function pullDown() {
   // 要素.addEventListener('イベント名', 関数)
   const pullDownButton = document.getElementById("lists")
   const pullDownParents = document.getElementById("pull-down")
+  const pullDownChild = document.querySelectorAll(".pull-down-list")
+  const currentList = document.getElementById("current-list")
 
-  pullDownButton.addEventListener('mouseover', function(){
+  this.addEventListener('mouseover', function(){
     // 要素.addEventListener('イベント名', 関数)
     this.setAttribute("style", "background-color:#FFBEDA;")
   })
@@ -14,7 +16,24 @@ window.addEventListener('load', function(){
   })
 
   pullDownButton.addEventListener('click', function() {
-    pullDownParents.setAttribute("style", "display:block;")
+    // プルダウンメニューの表示と非表示の設定
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
+      pullDownParents.setAttribute("style", "display:block;")
+    }
   })
 
-})
+  // コースの値を取得し表示する
+  pullDownChild.forEach(function(list) {
+    list.addEventListener('click', function() {
+      const value = list.innerHTML
+      console.log(value)
+      currentList.innerHTML = value
+    })
+  })
+}
+
+window.addEventListener('load', pullDown)
